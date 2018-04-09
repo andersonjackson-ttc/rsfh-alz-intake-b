499dea3bc48d4c084628d91733346cc6b598f14d
anderson jackson <anderson.jackson@tridenttech.edu>
2018-04-09 18:04:42 -0400

    Added the actual project files

---
+++ java/RoperALZIntake/src/edu/tridentech/rsfh/alzintake/Main.java
@@ -0,0 +1,17 @@
+package edu.tridentech.rsfh.alzintake;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+}

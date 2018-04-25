ae2a9971df2fd604744a374e0a8f0cdc9d559781
01hope10 <01hope10@T-800>
2018-04-25 13:38:21 -0400

    CSS version 2

--- java/RoperALZIntake/src/edu/tridentech/rsfh/alzintake/Main.java
+++ java/RoperALZIntake/src/edu/tridentech/rsfh/alzintake/Main.java
@@ -10,8 +10,8 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("rsfhCbriArcIntakeForm.fxml"));
-	    // Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("mainWindow.fxml"));
+		//Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("rsfhCbriArcIntakeForm.fxml"));
+	    Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("mainWindow.fxml"));
 
 	        Scene scene = new Scene(root);
 	     scene.getStylesheets().add(Main.class.getClassLoader().getResource("RoperIntake.css").toExternalForm());

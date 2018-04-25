4209e58a980a66071d55b43500d05965da364bed
leoparddude <38069233+leoparddude@users.noreply.github.com>
2018-04-25 00:56:45 -0400

    Add files via upload

---
+++ java/RoperALZIntake/resources/FXMLDocumentController.java
@@ -0,0 +1,71 @@
+/*
+ * see FXML variables commented out below
+ */
+package cbri_alz_intake_ui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+/**
+ *
+ * @author 5JLBt1
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Text actiontarget;
+    
+    @FXML
+    private void handleSubmitButtonAction(ActionEvent event) {
+        
+    }
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
+// @FXML private DatePicker intakeScreenDateTxtBx;
+//   @FXML private ComboBox intakeScreenByDrpDn;
+//   @FXML private TextField subjectFirstNameTxtBx;
+//   @FXML private TextField subjectMITxtBx;
+//   @FXML private TextField subjectLastNameTxtBx;
+//   @FXML private TextField subjectDOBTxtBx;
+//   @FXML private TextField subjectAddressTxtBx;
+//   @FXML private TextField subjectStateTxtBx;
+//   @FXML private TextField subjectZipTxtBx;
+//   @FXML private CheckBox hpoaStatusCkBxY;
+//   @FXML private CheckBox hpoaStatusCkBxN;
+//   @FXML private TextField subjectEmailTxtBx;
+//   @FXML private TextField subjectPhoneTxtBx;
+//   @FXML private TextField subjectCellTxtBx;
+//   @FXML private TextField subjectPcpTxtBx;
+//   @FXML private TextField subjectCityTxtBx;
+//   @FXML private TextField subjectSpecialistTxtBx;
+//   @FXML private TextField hpoaFirstNameTxtBx;
+//   @FXML private TextField hpoaLastNameTxtBx;
+//   @FXML private TextField hpoaPhoneTxtBx;
+//   @FXML private ComboBox subjectReferralDrpDn;
+//   @FXML private CheckBox studyPartnerStatusTglBtn;
+//   @FXML private TextField studyPartnerRelationTxtBx;
+//   @FXML private TextField studyPartnerPhoneTxtBx;
+//   @FXML private TextField studyPartnerEmailTxtBx;
+//   @FXML private TextField studyPartnerLastNameTxtBx;
+//   @FXML private TextField studyPartnerFirstNameTxtBx;
+//   @FXML private TextField hpoaSpousePhoneTxtBx;
+//   @FXML private TextField hpoaSpouseLastNameTxtBx;
+//   @FXML private TextField hpoaSpouseFirstNameTxtBx;
+//   @FXML private CheckBox hpoaMarriedStatusCkBxN;
+//   @FXML private CheckBox hpoaMarriedStatusCkBxY;
+//   @FXML private CheckBox hpoaChildStatusCkBxY;
+//   @FXML private CheckBox hpoaChildStatusCkBxN;
+//   @FXML private TextField hpoaChildFirstNameTxtBx;
+//   @FXML private TextField hpoaChildLastNameTxtBx;
+//   @FXML private TextField hpoaChildPhoneTxtBx;

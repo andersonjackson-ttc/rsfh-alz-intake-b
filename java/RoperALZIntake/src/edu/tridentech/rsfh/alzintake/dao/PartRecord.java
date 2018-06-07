a7b4bd8357f497675dbb48d1c4fa6d839159e748
Kevin Franks <k_franks@outlook.com>
2018-06-07 11:56:54 -0400

    Finished the PartRecord Interface for the short form

--- java/RoperALZIntake/src/edu/tridentech/rsfh/alzintake/dao/PartRecord.java
+++ java/RoperALZIntake/src/edu/tridentech/rsfh/alzintake/dao/PartRecord.java
@@ -10,11 +10,68 @@ import java.util.Date;
 
 public interface PartRecord  
 {
+	String getScreener();
+	void setScreener(String screen);
+	String getMiddleI();
+	void setMiddleI(String mi);
 	String getFirstName();
+	void setFirstName(String name);
 	String getLastName();
+	void setLastName(String name);
 	Date getDOB();
+	void setDOB(Date dob);
 	String getRace();
+	void setRace();
 	String getGender();
+	void setGender(String gender);
+	String getEmail();
+	void setEmail(String email);
+	String getReferral();
+	void setReferral(String ref);
+	String getAddress();
+	void setAddress(String address);
+	String getCity();
+	void setCity(String city);
+	String getState();
+	void setState(String state);
+	String getZip();
+	void setZip(String zip);
+	String getPhone();
+	void setPhone(String phone);
+	String getCell();
+	void setCell(String cell);
+	String getPcp();
+	void setPcp(String pcp);
+	String getSpec();
+	void setSpec(String spec);
+	boolean getPartner();
+	void setPartner(boolean prt);
+	String getPartFirstName();
+	void setPartFirstname(String name);
+	String getPartLastName();
+	void setPartLastName(String name);
+	String getPartPhone();
+	void setPartPhone(String phone);
+	String getPartEmail();
+	void setPartEmail(String email);
+	String getPartRel();
+	void setPartRel(String relation);
+	boolean getSpouse();
+	void setSpouse(boolean spo);
+	String getSpouseFistName();
+	void setSpouseFirstName(String name);
+	String getSpouseLastName();
+	void setSpouseLastName(String name);
+	String getSpousePhone();
+	void setSpousePhone(String phone);
+	boolean getChild();
+	void setChild(boolean child);
+	String getChildFirstName();
+	void setChildFirstName(String name);
+	String getChildLastName();
+	void setChildLastName(String name);
+	String getChildPhone();
+	void setChildPhone(String phone);
 	
 	
 }

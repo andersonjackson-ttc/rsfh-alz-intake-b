d0ca6298d3445ff1d19f92142f666268bacf9a97
Kevin Franks <k_franks@outlook.com>
2018-06-12 01:28:21 -0400

    DAO now will fill first page of SS

--- java/RoperALZIntake/src/edu/tridentech/rsfh/alzintake/dao/DataReadWrite.java
+++ java/RoperALZIntake/src/edu/tridentech/rsfh/alzintake/dao/DataReadWrite.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
@@ -17,6 +18,8 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 public class DataReadWrite 
 {
 	private static DataReadWrite dwr = null;
+	
+	
 	
 	
 	protected DataReadWrite()
@@ -47,6 +50,29 @@ public class DataReadWrite
 	{
 		String file = "RoperSpreadSheet.xlsx";
 		
+		HashMap<String, Integer> map = new HashMap<>();
+		
+		map.put("LastName", 1);
+		map.put("FirstName", 2);
+		map.put("DOB", 3);
+		map.put("Race", 4);
+		map.put("Gender", 5);
+		map.put("Address", 6);
+		map.put("Address2", 7);
+		map.put("City", 8);
+		map.put("State", 9);
+		map.put("Zip", 10);
+		map.put("Email", 11);
+		map.put("Phone", 12);
+		map.put("Status", 13);
+		map.put("Deseased", 14);
+		map.put("PCP", 15);
+		map.put("Spec", 16);
+		map.put("CurrentStudy", 17);
+		map.put("Referral", 18);
+		map.put("Mail", 19);
+		
+		
 		try
 		{
 			FileInputStream inputStream = new FileInputStream(new File(file));
@@ -58,28 +84,28 @@ public class DataReadWrite
 			
 			Row row = sheet.createRow(++rowCount);
 			
-			Cell cell = row.createCell(1);
+			Cell cell = row.createCell(map.get("LastName"));
 			cell.setCellValue(rd.getLastName());
 			
-			cell = row.createCell(2);
+			cell = row.createCell(map.get("FirstName"));
 			cell.setCellValue(rd.getFirstName());
 			
-			cell = row.createCell(3);
+			cell = row.createCell(map.get("DOB"));
 			cell.setCellValue(rd.getDOB());
 			
-			cell = row.createCell(5);
+			cell = row.createCell(map.get("Race"));
 			cell.setCellValue(rd.getRace());
 			
-			cell = row.createCell(6);
+			cell = row.createCell(map.get("Gender"));
 			cell.setCellValue(rd.getGender());
 			
-			cell = row.createCell(7);
+			cell = row.createCell(map.get("Address"));
 			cell.setCellValue(rd.getAddress());
 			
-			cell = row.createCell(8);
+			cell = row.createCell(map.get("Email"));
 			cell.setCellValue(rd.getEmail());
 			
-			cell = row.createCell(9);
+			cell = row.createCell(map.get("Phone"));
 			cell.setCellValue(rd.getPhone());
 			
 			//cell = row.createCell(15);
@@ -88,16 +114,16 @@ public class DataReadWrite
 			//cell = row.createCell(16);
 			//cell.setCellValue(rd.getStatus());
 			
-			cell = row.createCell(10);
+			cell = row.createCell(map.get("PCP"));
 			cell.setCellValue(rd.getPcp());
 			
-			cell = row.createCell(11);
+			cell = row.createCell(map.get("Spec"));
 			cell.setCellValue(rd.getSpec());
 			
-			cell = row.createCell(12);
+			cell = row.createCell(map.get("Referral"));
 			cell.setCellValue(rd.getReferral());
 			
-			//cell = row.createCell(13);
+			//cell = row.createCell(map.get("Mail"));
 			//cell.setCellValue(rd.getMailing());
 			
 			inputStream.close();
